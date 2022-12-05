090287822855331b4a3088699c6150cbf8ec446d
hwang <geungan9@gmail.com>
2022-12-05 15:12:21 +0900

    브랜치테스트2커밋을 한다면 어떨까 남아 있을까??

--- src/main/java/깃삭제연습/첫번째.java
+++ src/main/java/깃삭제연습/첫번째.java
@@ -5,6 +5,6 @@ public class 첫번째 {
     public static void main(String[] args) {
         System.out.println("첫번째 깃 추가이다");
         System.out.println("두번째 깃 추가이다.");
+        System.out.println("커밋을 진행 한다면 어떨까?");
     }
-
 }
