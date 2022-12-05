50aca5bee96f9b441ec06154ffc9fd7e03b34fca
hwang <geungan9@gmail.com>
2022-12-05 22:06:15 +0900

    마스터커밋 브랜치 이어지게 가능한지.

--- src/main/java/깃삭제연습/추가할부분.java
+++ src/main/java/깃삭제연습/추가할부분.java
@@ -1,11 +1,9 @@
 package 깃삭제연습;
 
-public class 첫번째 {
+public class 추가할부분 {
 
     public static void main(String[] args) {
-        System.out.println("첫번째 깃 추가이다");
-        System.out.println("두번째 깃 추가이다.");
-
         System.out.println("이부분 브랜치 연동하고 싶어");
     }
+
 }
