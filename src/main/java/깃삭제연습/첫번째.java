ee37e1aae2fc445cea2d14c70e37272a495ea0cf
hwang <geungan9@gmail.com>
2022-12-05 09:27:41 +0900

    브랜치 첫 내용추가

--- src/main/java/깃삭제연습/첫번째.java
+++ src/main/java/깃삭제연습/첫번째.java
@@ -5,6 +5,7 @@ public class 첫번째 {
     public static void main(String[] args) {
         System.out.println("첫번째 깃 추가이다");
         System.out.println("두번째 깃 추가이다.");
+        System.out.println("브랜치에서 작업한 내용이다.기존것에 문제가 되지 않는 지 확인하자.");
     }
 
 }
