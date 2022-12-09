2584f1cc1f3eab2325f597e6f2663fccc5d00f2a
hwang <geungan9@gmail.com>
2022-12-09 23:38:23 +0900

    브랜치2

--- src/main/java/깃삭제연습/첫번째.java
+++ src/main/java/깃삭제연습/첫번째.java
@@ -1,5 +1,7 @@
 package 깃삭제연습;
 
+import lombok.extern.slf4j.Slf4j;
+
 public class 첫번째 {
 
     public static void main(String[] args) {
@@ -7,5 +9,6 @@ public class 첫번째 {
         System.out.println("두번째 깃 추가이다.");
 
         System.out.println("이부분 브랜치 연동하고 싶어");
+        System.out.println("추가할부분커밋안하고.");
     }
 }
--- src/main/java/깃삭제연습/추가할부분.java
+++ src/main/java/깃삭제연습/추가할부분.java
@@ -4,6 +4,6 @@ public class 추가할부분 {
 
     public static void main(String[] args) {
         System.out.println("이부분 브랜치 연동하고 싶어");
+        System.out.println("추가할부분커밋안하고.");
     }
-
 }
